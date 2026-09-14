01db76782973d0cabd86b3cb26af251a4e033442
DmchFast <chdima28@gmail.com>
2026-09-15 01:05:09 +0300

    feat: add storage module for JSON load/save

---
+++ storage.py
@@ -0,0 +1,16 @@
+import json
+from typing import Any
+
+
+def load_json(filename: str) -> Any:
+    #Загрузка данные из JSON-файла
+    try:
+        with open(filename, "r", encoding="utf-8") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return None
+
+
+def save_json(filename: str, data: Any) -> None:
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
